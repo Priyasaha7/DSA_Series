d30b84ad2cfd8d2f9da676819f5d9d29dcdd94f5
Priyasaha7 <148933801+Priyasaha7@users.noreply.github.com>
2026-02-03 16:06:48 +0530

    add function to count negative values in array

--- Foundation/Basics/searchElement.js
+++ Foundation/Basics/searchElement.js
@@ -1,3 +1,5 @@
+// Write a function that searches for an element in an array and returns the index, if the element is not present then just return -1
+
 function searchElement(arr, x) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === x) {
@@ -6,3 +8,9 @@ function searchElement(arr, x) {
   }
   return -1;
 }
+
+let arr = [12, 23, 34, 56, 89];
+
+let result = searchElement(arr, 48);
+
+console.log(result);
