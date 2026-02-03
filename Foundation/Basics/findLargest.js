df32b08cca6bb0cedbc0054bb6d9e1e4cc2e17af
Priyasaha7 <148933801+Priyasaha7@users.noreply.github.com>
2026-02-03 16:22:10 +0530

    add function to find the largest  value in array

---
+++ Foundation/Basics/findLargest.js
@@ -0,0 +1,17 @@
+// Write a function that returns the largest number in an array
+
+function findLargest(arr) {
+  let x = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > x) {
+      x = arr[i];
+    }
+  }
+  return x;
+}
+
+let arr = [12, 22, 56, 11, 89, 0, 67];
+
+let result = findLargest(arr);
+
+console.log(result);
