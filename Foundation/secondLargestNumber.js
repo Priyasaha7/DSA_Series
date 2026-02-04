1dfe9d2b26ff8df8e4e4f851aaf7570f471ef2a7
Priyasaha7 <148933801+Priyasaha7@users.noreply.github.com>
2026-02-04 23:25:06 +0530

    handled error for findinf the second largest element in an array

--- Foundation/secondLargestNumber.js
+++ Foundation/secondLargestNumber.js
@@ -1,58 +1,59 @@
 //Write a function secondLargest(arr) that returns the second largest distinct number in an array.
 
-function secondLargestNumber(arr) {
-  if (!Array.isArray(arr)) {
-    return false;
-  }
+// function secondLargestNumber(arr) {
+//   if (!Array.isArray(arr)) {
+//     return false;
+//   }
 
-  if (arr.length < 2) {
-    return null;
-  }
+//   if (arr.length < 2) {
+//     return null;
+//   }
 
-  let x = -Infinity;
-  let y = -Infinity;
+//   let x = -Infinity;
+//   let y = -Infinity;
 
-  for (let i = 0; i < arr.length; i++) {
-    if (typeof arr[i] !== "number" || !Number.isFinite(arr[i])) {
-      return false;
-    }
-  }
+//   for (let i = 0; i < arr.length; i++) {
+//     if (typeof arr[i] !== "number" || !Number.isFinite(arr[i])) {
+//       return false;
+//     }
+//   }
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > x) {
-      y = x;
-      x = arr[i];
-    } else if (arr[i] > y && arr[i] !== x) {
-      y = arr[i];
-    }
-  }
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > x) {
+//       y = x;
+//       x = arr[i];
+//     } else if (arr[i] > y && arr[i] !== x) {
+//       y = arr[i];
+//     }
+//   }
 
-  if (y === -Infinity) {
-    return null;
-  }
+//   if (y === -Infinity) {
+//     return null;
+//   }
 
-  return y;
-}
+//   return y;
+// }
 
 //LEETCODE
 
-// var secondHighest = function(s) {
-//  let max=-1;
-//  let second=-1;
-//   for (let i = 0; i < s.length; i++) {
-//     let ch = s[i];
+//Given an alphanumeric string s, return the second largest numerical digit that appears in s, or -1 if it does not exist. An alphanumeric string is a string consisting of lowercase English letters and digits.
 
-//     if( ch >= "0" || ch <= "9"){
-//         let num = Number(ch)
+var secondHighest = function (s) {
+  let max = -1;
+  let second = -1;
+  for (let i = 0; i < s.length; i++) {
+    let ch = s[i];
 
-//         if( num>max){
-//             second = max;
-//             max = num
-//         }else if(num>second && num !== max){
-//             second = num
-//         }
-//     }
-//   }
-//   return second ;
+    if (ch >= "0" || ch <= "9") {
+      let num = Number(ch);
 
-// };
+      if (num > max) {
+        second = max;
+        max = num;
+      } else if (num > second && num !== max) {
+        second = num;
+      }
+    }
+  }
+  return second;
+};
