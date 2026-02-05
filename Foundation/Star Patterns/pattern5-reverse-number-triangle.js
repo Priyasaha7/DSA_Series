a4dfc0c935f13ef7a9f97d7ab453fbc2f3bd20db
Priyasaha7 <148933801+Priyasaha7@users.noreply.github.com>
2026-02-05 18:03:49 +0530

    feat: add reverse number triangle pattern

---
+++ Foundation/Star Patterns/pattern5-reverse-number-triangle.js
@@ -0,0 +1,12 @@
+// Pattern 5: Reverse Right-Angled Triangle of Increasing Numbers
+// Numbers decrease row by row.
+
+let n = 4;
+
+for (let i = 0; i < n; i++) {
+  let row = "";
+  for (let j = 0; j < n - i; j++) {
+    row += j + 1 + " ";
+  }
+  console.log(row);
+}
