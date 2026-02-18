9698bfe80e9ca23c76ed35daf77baf3ab8f02b2b
Priyasaha7 <148933801+Priyasaha7@users.noreply.github.com>
2026-02-18 23:12:42 +0530

    feat: compute nth fibonacci number using recursion

---
+++ Recursion/ibonacciRecursion.js
@@ -0,0 +1,10 @@
+// Find the nth Fibonacci number using recursion.
+
+function fibonacci(n) {
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+console.log(fibonacci(6));
